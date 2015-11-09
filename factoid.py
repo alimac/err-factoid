2d522ac39b70c4401bf42724fe7a2e8a84ae1772
Alina Mackenzie <alina.mackenzie@gmail.com>
2015-11-09 07:20:01 -0600

    Implement factoid store

--- factoid.py
+++ factoid.py
@@ -5,8 +5,25 @@ from errbot import BotPlugin, botcmd, re_botcmd
 class Factoid(BotPlugin):
     """Factoid plugin"""
 
+    factoid_store = {}
+
     @re_botcmd(pattern=r'^((\w+\s??){1,3}) is (.+$)', prefixed=False, flags=re.IGNORECASE)
     def factoid(self, message, match):
         factoid = match.group(1)
         content = match.group(3)
-        return "You said: %s is %s" % (format(factoid), format(content))
+
+        self.factoid_store[factoid] = content
+        self['FACTOID'] = self.factoid_store
+
+        return "Got it, %s is %s" % (format(factoid), format(content))
+
+    @re_botcmd(pattern=r'^((\w+\s??){1,3})\?$', prefixed=False, flags=re.IGNORECASE)
+    def tell_factoid(self, message, match):
+        factoid = match.group(1)
+        self.factoid_store = self['FACTOID']
+
+        if self.factoid_store[factoid]:
+            return "%s is %s" % (
+                factoid,
+                format(self.factoid_store[factoid])
+            )
