fe172703dd0f09b3e56805a3d4d35adcffd09f01
Alina Mackenzie <alina.mackenzie@gmail.com>
2015-11-14 07:48:59 -0600

    Add tests

---
+++ test_factoid.py
@@ -0,0 +1,46 @@
+import os
+import unittest
+import factoid
+from errbot.backends.test import testbot
+from errbot import plugin_manager
+
+class TestFactoid(object):
+    extra_plugin_dir = '.'
+
+    def test_list_factoids(self, testbot):
+        testbot.push_message('!list factoids')
+        assert 'I have not learned any factoids yet.' in testbot.pop_message()
+
+        testbot.push_message('!water is wet')
+        assert 'Got it, water is wet' in testbot.pop_message()
+
+        testbot.push_message('!list factoids')
+        assert "I'm Err! I know about:" in testbot.pop_message()
+        assert 'water' in testbot.pop_message()
+
+    def test_learn_factoid(self, testbot):
+        testbot.push_message('!water is wet')
+        assert 'Got it, water is wet' in testbot.pop_message()
+
+        testbot.push_message('water?')
+        assert 'water is wet' in testbot.pop_message()
+
+        testbot.push_message('!water is hot')
+        assert 'I already know about water, but you can tell me to forget it.' in testbot.pop_message()
+
+    def test_tell_factoid_unknown(self, testbot):
+        testbot.push_message('sky?')
+        assert '/me does not know about sky' in testbot.pop_message()
+
+    def test_forget_factoid(self, testbot):
+        testbot.push_message('!sky is blue')
+        assert 'Got it, sky is blue' in testbot.pop_message()
+
+        testbot.push_message('sky?')
+        assert 'sky is blue' in testbot.pop_message()
+
+        testbot.push_message('!forget sky')
+        assert 'OK, I forgot about sky' in testbot.pop_message()
+
+        testbot.push_message('!list factoids')
+        assert 'I have not learned any factoids yet.' in testbot.pop_message()
